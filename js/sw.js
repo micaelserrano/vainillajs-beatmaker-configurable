18652a1cc30a5919d71f170465072850bcbc70bf
Micael <micael.serrano@yahoo.com.ar>
2019-04-15 22:00:01 -0300

    sw to es5

--- js/sw.js
+++ js/sw.js
@@ -1,42 +1,44 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  './css/style.min.css',
-  './js/sw.js',
-  './js/index.min.js'
-];
-self.addEventListener('install', event => {
+var PRECACHE = 'precache-v1';
+var RUNTIME = 'runtime';
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+      .then(function(cache ) {
+        cache.addAll([
+        'index.html',
+        './', // Alias for index.html
+        './css/style.min.css',
+        './js/sw.js',
+        './js/index.min.js'
+      ]);
+  }).then(function() {
+    self.skipWaiting();
+  }));
 });
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+self.addEventListener('activate', function(event) {
+  var currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
+    caches.keys().then(function(cacheNames) {
+      return cacheNames.filter(function(cacheName) {
+        return !currentCaches.includes(cacheName) 
+      });
+    }).then(function(cachesToDelete) {
+      return Promise.all(cachesToDelete.map(function(cacheToDelete) {
         return caches.delete(cacheToDelete);
       }));
-    }).then(() => self.clients.claim())
-  );
+    }).then(function() {self.clients.claim() }));
 });
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   // if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
+      caches.match(event.request).then(function(cachedResponse) {
         if (cachedResponse) {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
+        return caches.open(RUNTIME).then(function(cache) {
+          return fetch(event.request).then(function(response) {
+            return cache.put(event.request, response.clone()).then(function() {
               return response;
             });
           });
